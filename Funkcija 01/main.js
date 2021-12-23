4c5f851eccc9e2f0bc531a18bd9f69b72117c4b1
skoroluka <96566661+skoroluka@users.noreply.github.com>
2021-12-23 08:35:38 +0100

    Add files via upload

---
+++ Funkcija 01/main.js
@@ -0,0 +1,36 @@
+// JavaScript funkcije
+
+// definiramo funkciju
+function mojaFunkcija () {
+
+    alert("Ovo je moja poruka iz naše funkcije");
+
+}
+
+// pozivamo funkciju
+//mojaFunkcija();
+
+function zbroji(a,b) {
+
+    c = a + b;
+    alert("Zbroj a i b je jednako:" + c);
+
+}
+
+//zbroji(4,3);
+//zbroji(53,69);
+
+
+let x = 0;
+
+function doseg() {
+
+    let x = 100;
+
+    alert(x);
+}
+
+alert(x); 
+
+doseg();
+
---
+++ Funkcije 02/main.js
@@ -0,0 +1,14 @@
+// Funkcija sa parametrima
+
+// Program koji pozdravlja korisnika
+function pozdrav(ime) {
+
+    alert("Dobar dan " + ime + " :)");
+
+}
+
+// Pitaj korisnika za ime
+let ime = prompt("Unesi svoje ime: ");
+
+// Pozivanje funkcije
+pozdrav(ime);
